cf932a8aac2b402f241330a33c9e58129539a0be
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2020-09-01 10:04:40 +0530

    Upload 1.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+# Program to find the ASCII value of a character
+
+print("************")
+print("*--ascii--*")
+print("***********")
+print("--@Ro706@--")
+
+
+ch = input("@ro706~>>> ")
+
+print("The ASCII value of char " + ch + " is: ",ord(ch))
+                      
